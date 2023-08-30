97d886bf8a43b40881e04a411156937fae3698a8
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2023-08-30 20:33:57 -0300

    Update and rename a.java to Main.java

---
+++ TDD1/tdd1/src/main/java/tdd1/Main.java
@@ -0,0 +1,14 @@
+package tdd1;
+
+//import java.io.*;
+import java.util.*;
+//import java.time.LocalTime;
+
+public class Main {
+    public static void main(String[] args) {
+
+      System.out.println("Hallo World");
+        
+  }
+}
+
