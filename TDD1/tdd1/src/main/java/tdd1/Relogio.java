fb381e724dab1d9db3773d8c08dd02a5c0ddafd9
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2023-08-30 20:44:54 -0300

    Update Relogio.java

--- TDD1/tdd1/src/main/java/tdd1/Relogio.java
+++ TDD1/tdd1/src/main/java/tdd1/Relogio.java
@@ -14,4 +14,47 @@ public class Relogio {
         this.tempoInicial = LocalTime.of(0, 0, 0);
         this.tempoAtual = LocalTime.of(0, 0, 0);
     }
+
+    public LocalTime definirHora(int horas, int minutos, int segundos) {
+        if (horas < 0 || horas > 23 || minutos < 0 || minutos > 59 || segundos < 0 || segundos > 59) {
+            throw new IllegalArgumentException("Invalid time values");
+        }
+        this.tempoAtual = LocalTime.of(horas, minutos, segundos);
+        //System.out.println(this.tempoAtual + " Hora definida!");
+        return tempoAtual;
+    }
+
+    public String obterHora() {
+        return tempoAtual.toString();
+    }
+
+    public void reiniciarMeiaNoite() {
+        tempoAtual = LocalTime.of(0, 0, 0);
+    }
+
+    public void iniciarCronometro() {
+        tempoInicial = LocalTime.now();
+    }
+
+    public Duration obterTempoDecorrido() {
+        return Duration.between(tempoInicial, tempoAtual);
+    }
+
+    public void atualizarTempo(int horas, int minutos, int segundos) {
+        tempoAtual =  definirHora(horas, minutos, segundos);
+        System.out.println(tempoAtual + " tempo atualizado!");
+    }
+
+    public String imprimirFormato24h() {
+        return tempoAtual.toString();
+    }
+
+    public String imprimirFormatoAMPM() {
+        return tempoAtual.format(DateTimeFormatter.ofPattern("hh:mm:ss a"));
+    }
+
+    public void atualizarTempoSistema() {
+        tempoAtual = LocalTime.now();
+    }
+
 }
