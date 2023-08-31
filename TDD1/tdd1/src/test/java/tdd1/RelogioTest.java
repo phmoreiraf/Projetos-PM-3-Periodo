55f724daf0a0cb8f7bb6d1e57a53897450293e24
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2023-08-30 21:28:17 -0300

    Update RelogioTest.java

--- TDD1/tdd1/src/test/java/tdd1/RelogioTest.java
+++ TDD1/tdd1/src/test/java/tdd1/RelogioTest.java
@@ -20,4 +20,68 @@ public class RelogioTest {
         assertEquals("10:30:10", relogio.obterHora());
     }
 
+    /*@Test
+    public void testDefinirHoraInvalida() {
+        assertThrows(IllegalArgumentException.class, () -> relogio.definirHora(25, 0, 0));
+        assertThrows(IllegalArgumentException.class, () -> relogio.definirHora(10, 61, 0));
+    }*/
+
+    @Test
+    public void testReiniciarMeiaNoite() {
+        relogio.definirHora(14, 30, 00);
+        relogio.reiniciarMeiaNoite();
+        assertEquals("00:00", relogio.obterHora());
+    }
+
+    @Test
+    public void testIniciarCronometro() {
+        LocalTime tempoInicial;
+        tempoInicial = LocalTime.now();
+        relogio.iniciarCronometro();
+        assertFalse(tempoInicial.isBefore(tempoInicial));
+    }
+
+        @Test
+        public void testObterTempoDecorrido() {
+
+            Relogio tempoDecorrido = new Relogio();
+            tempoDecorrido.iniciarCronometro();
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Duration tempoDecorridoObtido = tempoDecorrido.obterTempoDecorrido();
+            assertFalse(tempoDecorridoObtido.toMillis() >= 0);
+        }
+
+
+    @Test
+    public void testAtualizarTempo() {
+
+        relogio.definirHora(10, 0, 0);
+        relogio.atualizarTempo(1, 30, 15);
+        assertEquals("01:30:15", relogio.obterHora());
+    }
+
+    @Test
+    public void testImprimirFormato24h() {
+        relogio.definirHora(15, 45, 30);
+        assertEquals("15:45:30", relogio.imprimirFormato24h());
+    }
+
+    @Test
+    public void testImprimirFormatoAMPM() {
+        relogio.definirHora(18, 15, 00);
+        assertEquals("06:15:00 PM", relogio.imprimirFormatoAMPM());
+    }
+
+    @Test
+    public void testAtualizarTempoSistema() {
+        relogio.atualizarTempoSistema();
+        assertTrue(relogio.obterHora().compareTo("00:00:00") > 0);
+    }
+
 }
